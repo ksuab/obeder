6aad45c6c09054c44b4549af58005ceff96294a2
Avrel1a <olt.evgenia@gmail.com>
2025-07-25 13:06:02 +0300

    t

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,6 @@
-from bot.handlers import register_all_handlers
+from bot.handlers import register_all_handlersfrom .handlers import register_all_handlers
+from .keyboards import *
+from .states import *
+from .utils import *
+
+__all__ = ['register_all_handlers']
